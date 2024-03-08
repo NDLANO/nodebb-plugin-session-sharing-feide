0652dc8334f8dad362074348d7b8a87788e823c2
Even Kronen Johansen <even__kj@hotmail.com>
2024-03-08 07:49:42 +0100

    Add null check (#17)
    * Add null check

--- library.js
+++ library.js
@@ -555,19 +555,21 @@ const getFeideUser = async (token, validRoles) => {
       userInfo: transformedUserInfo,
     };
   }
-  if (!validRoles.some((role) => feideInfo.role === role)) {
-    winston.warn('[Feide-authentication] User role is not valid.', {
-      userRole: feideInfo.role,
-      validRoles,
-    });
-  }
-  if (feideInfo.arenaEnabled === false) {
-    winston.warn(
-      '[Feide-authentication] User profile has arena enabled set to false.',
-      {
-        arenaEnabled: feideInfo.arenaEnabled,
-      },
-    );
+  if (feideInfo !== null) {
+    if (!validRoles.some((role) => feideInfo.role === role)) {
+      winston.warn('[Feide-authentication] User role is not valid.', {
+        userRole: feideInfo.role,
+        validRoles,
+      });
+    }
+    if (feideInfo.arenaEnabled === false) {
+      winston.warn(
+        '[Feide-authentication] User profile has arena enabled set to false.',
+        {
+          arenaEnabled: feideInfo.arenaEnabled,
+        },
+      );
+    }
   }
   return { isValidMember: false };
 };
